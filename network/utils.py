4f3b92365782e9dc88047256b0397b11e3905ef1
Tony <anthony@amartinez.cc>
2019-01-18 12:55:58 -0800

    update notebook

--- network/utils.py
+++ network/utils.py
@@ -8,6 +8,8 @@ Created on Fri Jan 18 11:25:40 2019
 
 import torch
 import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Flatten(nn.Module):
     def forward(self, input):
@@ -20,3 +22,23 @@ def accuracy(out, labels):
     labels= torch.argmax(labels.data)
     total += torch.sum(predicted == labels.data)
     return total.cpu().detach().numpy()/size
+
+
+#https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+def imshow_transform(image_in, title=None):
+    """Imshow for Tensor."""
+    img = np.rollaxis(image_in.squeeze().cpu().detach().numpy(),0,3)
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    img = std * img + mean
+    img = np.clip(img, 0, 1)
+    plt.imshow(img)
+    return img
+
+#https://www.fast.ai/
+#fastai code snippet
+class SaveFeatures():
+    features=None
+    def __init__(self,m): self.hook = m.register_forward_hook(self.hook_fn)
+    def hook_fn(self, module, input, output): self.features = output
+    def remove(self): self.hook.remove()
